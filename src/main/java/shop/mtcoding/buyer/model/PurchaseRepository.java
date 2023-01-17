e720a77eb42f9256d5bfed830b82feb625dab6ac
hahahahahaha <zzzz@naver.com>
2023-01-17 18:50:52 +0900

    01.17 진짜로 복습 Repository 설정까지 진짜 끝끝!

--- src/main/java/shop/mtcoding/buyer/model/PurchaseRepository.java
+++ src/main/java/shop/mtcoding/buyer/model/PurchaseRepository.java
@@ -1,5 +1,20 @@
 package shop.mtcoding.buyer.model;
 
-public interface PurchaseRepository {
+import java.util.List;
+
+public interface PurchaseRepository { // CRUD
+    // Create
+    public int insert(int userId, int productId);
+
+    // Read
+    public List<Purchase> findAll();
+
+    public Purchase findById();
+
+    // Update
+    public int updateById(int userId, int productId);
+
+    // Delete
+    public int deleteById(int id);
 
 }
